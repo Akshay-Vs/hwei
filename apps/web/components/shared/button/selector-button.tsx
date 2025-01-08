52f516b353ecaf4deddf6bd22a957f2fffd21ad0
Akshay Vs <me.akshay.vs@gmail.com>
2025-01-08 20:22:55 +0530

    feat: add site visits

--- apps/web/components/shared/button/selector-button.tsx
+++ apps/web/components/shared/button/selector-button.tsx
@@ -32,7 +32,7 @@ const SelectorButton = ({
 				'h-12 min-w-16 px-6 shadow-none text-base font-medium transition-all duration-300',
 				selected
 					? 'border-accent/60 border-2 bg-accent/40 hover:bg-accent/60 text-secondary'
-					: 'border-secondary/60 border-2 bg-accent/5 hover:bg-secondary/90 text-secondary hover:text-white',
+					: 'border-secondary/60 border-2 bg-accent/5 hover:bg-secondary/90 focus-visible:bg-secondary/90 text-secondary focus-visible:text-white hover:text-white',
 				className
 			)}
 		>
---
+++ apps/web/components/specific/dashboard/dashboard.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SiteVisits from './components/site-visits';
+
+const Dashboard = () => {
+	return (
+		<div className="full">
+			<SiteVisits />
+		</div>
+	);
+};
+
+export default Dashboard;
