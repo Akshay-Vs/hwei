d6d9b89653cf4eb2f7c227863022bbd66d5de53f
Akshay Vs <me.akshay.vs@gmail.com>
2025-01-08 12:08:40 +0530

    feat: improve keyboard focusability

--- packages/ui/src/shadcn/button.tsx
+++ packages/ui/src/shadcn/button.tsx
@@ -4,7 +4,7 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import { cn } from '@/src/utils/cn';
 
 const buttonVariants = cva(
-	'inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-base font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50',
+	'inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-base font-medium transition-colors focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 focus:outline-2 focus:border-accent/20 focus:outline-dotted focus:outline-secondary',
 	{
 		variants: {
 			variant: {
