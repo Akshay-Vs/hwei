b9f379e1de1998feef01a657fe25346d5a0177d2
Akshay Vs <me.akshay.vs@gmail.com>
2025-01-11 00:03:03 +0530

    feat: add info-card component

--- apps/web/components/specific/products/components/monthly-sales/monthly-sales.tsx
+++ apps/web/components/specific/products/components/monthly-sales/monthly-sales.tsx
@@ -1,18 +1,15 @@
-import { ArrowUpRight } from 'lucide-react';
 import React from 'react';
-
-import { Card } from '@hwei/ui/shadcn/card';
+import InfoCard from '@/components/shared/card/info-card/info-card';
 
 const MonthlySales = () => {
 	return (
-		<Card className="h-full w-full flex items-center justify-evenly flex-col gap-5">
-			<h2 className="text-2xl font-medium">Monthly Sales</h2>
-			<p className="text-5xl font-medium">240</p>
-			<p className="text-base font-medium inline-flex justify-center items-center w-3/2 gap-2">
-				<ArrowUpRight className="inline-block w-8 h-8 text-green-500" />
-				47% increase from last month
-			</p>
-		</Card>
+		<InfoCard
+			title="Monthly Sales"
+			value="240"
+			change="increase"
+			affect="positive"
+			changeRate={10}
+		/>
 	);
 };
 
