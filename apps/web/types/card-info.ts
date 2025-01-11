52b08925980fb3a1e6797247202e59434f2e5126
Akshay Vs <me.akshay.vs@gmail.com>
2025-01-11 19:17:45 +0530

    Update apps/web/types/card-info.ts
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- apps/web/types/card-info.ts
+++ apps/web/types/card-info.ts
@@ -2,7 +2,7 @@ export type TCardInfo = {
   value: string | number;
   showChange?: boolean;
   description?: string;
-  affect?: 'positive' | 'negetive' | 'neutral';
+  affect?: 'positive' | 'negative' | 'neutral';
   change?: 'increase' | 'decrease' | 'neutral';
   changeRate?: number;
 }
