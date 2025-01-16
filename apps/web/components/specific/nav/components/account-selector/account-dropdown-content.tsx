b923c6c7cd3c3dc085a48adb5cbcb524ad25e3d1
Akshay Vs <me.akshay.vs@gmail.com>
2025-01-16 23:52:48 +0530

    feat: add accounts dropdown

---
+++ apps/web/components/specific/nav/components/account-selector/account-dropdown-content.tsx
@@ -0,0 +1,58 @@
+import Dropdown from '@/components/shared/dropdown/dropdown';
+import { Button } from '@hwei/ui/shadcn/button';
+import { Separator } from '@hwei/ui/shadcn/separator';
+import Image from 'next/image';
+import React from 'react';
+
+const AccountDropdownContent = ({ isOpen }: { isOpen: boolean }) => {
+	const actions = [
+		{
+			label: 'My Profile',
+			action: () => {},
+		},
+		{
+			label: 'Add Account',
+			action: () => {},
+		},
+		{
+			label: 'Switch Account',
+			action: () => {},
+		},
+		{
+			label: 'Log Out',
+			action: () => {},
+		},
+	];
+
+	return (
+		<Dropdown isOpen={isOpen}>
+			<div className="full center flex-col gap-1 text-center">
+				<Image
+					src="https://utfs.io/f/u628d5y0J6C1JoULTTGePIhzvZk7l1rq9wNsymFSYQAnLOT0"
+					width={100}
+					height={100}
+					alt="Profile"
+					className="w-20 h-20 rounded-full object-center object-cover"
+				/>
+				<p className="text-xl font-medium">Evelin Violet</p>
+				<p className="text-base font-normal text-secondary">evelin@gmail.com</p>
+			</div>
+			<Separator className="opacity-30" />
+
+			<div className="grid grid-cols-2 gap-2 mt-2">
+				{actions.map((action) => (
+					<Button
+						variant="outline"
+						className="h-12 w-full bg-background flex-1 hover:bg-secondary/10"
+						key={action.label}
+						onClick={action.action}
+					>
+						<p className="text-sm font-semibold text-black">{action.label}</p>
+					</Button>
+				))}
+			</div>
+		</Dropdown>
+	);
+};
+
+export default AccountDropdownContent;
--- apps/web/components/specific/nav/components/account-selector/account-selector.tsx
+++ apps/web/components/specific/nav/components/account-selector/account-selector.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+'use client';
+import React, { useState } from 'react';
 import Image from 'next/image';
 
 import { ChevronDown } from 'lucide-react';
@@ -9,33 +10,45 @@ import {
 	TooltipTrigger,
 } from '@hwei/ui/shadcn/tooltip';
 
+import AccountDropdownContent from './account-dropdown-content';
+
 const AccountSelector = () => {
+	const [isOpen, setIsOpen] = useState(false);
+
+	const toggleDropdown = () => {
+		setIsOpen((prev) => !prev);
+	};
+
 	return (
-		<div className="border-2 border-highlight h-fit w-72 p-1 rounded-base flex justify-between items-center cursor-pointer">
-			<Image
-				src="https://utfs.io/f/u628d5y0J6C1JoULTTGePIhzvZk7l1rq9wNsymFSYQAnLOT0"
-				width={86}
-				height={86}
-				alt="Profile"
-				className="w-16 h-16 rounded-full object-center object-cover"
-			/>
+		<div className="relative h-fit w-fit z-50">
+			<div className="border-2 border-highlight h-fit w-72 p-1 rounded-base flex justify-between items-center cursor-pointer">
+				<Image
+					src="https://utfs.io/f/u628d5y0J6C1JoULTTGePIhzvZk7l1rq9wNsymFSYQAnLOT0"
+					width={86}
+					height={86}
+					alt="Profile"
+					className="w-16 h-16 rounded-full object-center object-cover"
+				/>
 
-			<div>
-				<p className="text-sm font-normal">Welcome</p>
-				<p className="text-lg font-medium">Evelin Violet</p>
+				<div>
+					<p className="text-sm font-normal">Welcome</p>
+					<p className="text-lg font-medium">Evelin Violet</p>
+				</div>
+
+				<Tooltip>
+					<TooltipTrigger asChild>
+						<IconButton
+							icon={<ChevronDown className="text-inherit" />}
+							ariaLabel="My Account"
+							onClick={toggleDropdown}
+						/>
+					</TooltipTrigger>
+					<TooltipContent>
+						<p className="text-sm font-semibold">My Account</p>
+					</TooltipContent>
+				</Tooltip>
 			</div>
-
-			<Tooltip>
-				<TooltipTrigger asChild>
-					<IconButton
-						icon={<ChevronDown className="text-inherit" />}
-						ariaLabel="Switch Account"
-					/>
-				</TooltipTrigger>
-				<TooltipContent>
-					<p className="text-sm font-semibold">Switch Account</p>
-				</TooltipContent>
-			</Tooltip>
+			<AccountDropdownContent isOpen={isOpen} />
 		</div>
 	);
 };
