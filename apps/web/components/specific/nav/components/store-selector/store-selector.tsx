7a61a137196f072eafa2927302e9ce13689d17bc
Akshay Vs <me.akshay.vs@gmail.com>
2025-01-17 20:42:41 +0530

    feat(web): add store selector dropdown

--- apps/web/components/specific/nav/components/store-selector/store-selector.tsx
+++ apps/web/components/specific/nav/components/store-selector/store-selector.tsx
@@ -1,3 +1,4 @@
+'use client';
 import { Button } from '@hwei/ui/shadcn/button';
 import {
 	Tooltip,
@@ -5,26 +6,44 @@ import {
 	TooltipTrigger,
 } from '@hwei/ui/shadcn/tooltip';
 import { ChevronDown } from 'lucide-react';
-import React from 'react';
+import React, { useState } from 'react';
+import StoreSelectorDropdown from './store-selector-dropdown';
+import { DropdownBackdrop } from '@/components/shared/dropdown/dropdown';
 
 const StoreSelector = () => {
+	const [isOpen, setIsOpen] = useState(false);
+
+	const toggleDropdown = () => {
+		setIsOpen((prev) => !prev);
+	};
+
 	return (
-		<Tooltip>
-			<TooltipTrigger asChild>
-				<Button className="w-fit min-w-32 max-w-52 h-16 py-0 px-8 flex items-center justify-center bg-secondary">
-					<p className="text-lg font-semibold text-center line-clamp-1">
-						My Store
-					</p>
-					<ChevronDown
-						className="text-inherit mt-1"
-						aria-label="Switch Stores"
-					/>
-				</Button>
-			</TooltipTrigger>
-			<TooltipContent>
-				<p className="text-sm font-semibold">Switch Store</p>
-			</TooltipContent>
-		</Tooltip>
+		<>
+			<DropdownBackdrop isOpen={isOpen} setIsOpen={setIsOpen} />
+			<div className="relative h-fit w-fit z-50">
+				<Tooltip>
+					<TooltipTrigger asChild>
+						<Button
+							className="w-fit min-w-32 max-w-52 h-16 py-0 px-8 flex items-center justify-center bg-secondary"
+							onClick={toggleDropdown}
+						>
+							<p className="text-lg font-semibold text-center line-clamp-1">
+								My Store
+							</p>
+							<ChevronDown
+								className="text-inherit mt-1"
+								aria-label="Switch Stores"
+							/>
+						</Button>
+					</TooltipTrigger>
+					<TooltipContent>
+						<p className="text-sm font-semibold">Switch Store</p>
+					</TooltipContent>
+
+					<StoreSelectorDropdown isOpen={isOpen} />
+				</Tooltip>
+			</div>
+		</>
 	);
 };
 
