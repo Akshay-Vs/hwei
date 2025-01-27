3a8a6d97ae1e2e6acbab2f0fb94d74584b010d17
Akshay Vs <me.akshay.vs@gmail.com>
2025-01-27 21:55:41 +0530

    feat: add product-editor page

---
+++ apps/web/app/(pages)/products/editor/page.tsx
@@ -0,0 +1,8 @@
+import ProductEditor from '@/components/specific/product-editor/product-editor';
+import React from 'react';
+
+const ProductEditorPage = () => {
+	return <ProductEditor />;
+};
+
+export default ProductEditorPage;
