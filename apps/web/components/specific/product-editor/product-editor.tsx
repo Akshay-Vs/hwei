71b5ca62de460f36cc20f3ef4065972673098f3a
Akshay Vs <me.akshay.vs@gmail.com>
2025-01-29 16:30:30 +0530

    feat: add product-editor -/prototype

---
+++ apps/web/components/specific/product-editor/product-editor.tsx
@@ -0,0 +1,29 @@
+'use client';
+import { Form } from '@hwei/ui/shadcn/form';
+import React from 'react';
+import ProductMetadata from './components/product-metadata';
+import { useProductForm } from '@/hooks/use-product-form';
+import ProductPrice from './components/product-price';
+import EditorPreview from './components/editor-preview';
+import ProductCategory from './components/product-category';
+
+const ProductEditor = () => {
+	const { form } = useProductForm();
+	return (
+		<div className="flex flex-col gap-4 full">
+			<Form {...form}>
+				<form className="flex gap-4 w-full">
+					<div className="flex flex-col gap-4 w-1/2">
+						<ProductMetadata />
+						<ProductPrice />
+						<ProductCategory />
+					</div>
+
+					<EditorPreview />
+				</form>
+			</Form>
+		</div>
+	);
+};
+
+export default ProductEditor;
