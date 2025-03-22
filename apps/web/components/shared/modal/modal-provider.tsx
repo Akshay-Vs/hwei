9184f2ce95933ff9f9c5fc3c0091e9dd5cfdc0b7
Akshay-Vs <me.akshay.vs@gmail.com>
2025-03-23 00:02:14 +0530

    feat: add VariantItemModal to ModalProvider for improved modal management

--- apps/web/components/shared/modal/modal-provider.tsx
+++ apps/web/components/shared/modal/modal-provider.tsx
@@ -2,12 +2,14 @@
 import React, { Fragment } from 'react';
 import StoreModal from './store-modal';
 import VariantModal from './variant-modal';
+import VariantItemModal from './variant-item-modal';
 
 const ModalProvider = () => {
 	return (
 		<Fragment>
 			<StoreModal />
 			<VariantModal />
+			<VariantItemModal />
 		</Fragment>
 	);
 };
