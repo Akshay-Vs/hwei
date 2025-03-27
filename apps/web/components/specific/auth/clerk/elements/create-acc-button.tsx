f09fd3578308b05c7450c8fe7b2206e895f34a89
Akshay-Vs <me.akshay.vs@gmail.com>
2025-03-28 00:13:01 +0530

    feat: add CreateAccButton component for account creation navigation

---
+++ apps/web/components/specific/auth/clerk/elements/create-acc-button.tsx
@@ -0,0 +1,8 @@
+import { Link } from '@clerk/elements/common';
+import React from 'react';
+
+const CreateAccButton = () => {
+	return <Link navigate="sign-up">Create new account</Link>;
+};
+
+export default CreateAccButton;
