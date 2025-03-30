db9bde4bd0411bc3b253944e54f44bd2e5880f02
Akshay-Vs <me.akshay.vs@gmail.com>
2025-03-31 00:50:18 +0530

    chore: remove unnecessary console.log from middleware

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -1,5 +1,4 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
-console.log("Middleware hit")
 
 const isPublicRoute = createRouteMatcher(['/auth(.*)', '/api/uploadthing(.*)'])
 
