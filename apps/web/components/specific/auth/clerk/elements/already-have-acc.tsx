7770c9814b0c428677fa54dea77f1a408cc59f60
Akshay-Vs <me.akshay.vs@gmail.com>
2025-03-31 00:50:03 +0530

    fix: fallback to default auth routes if env vars are not set

--- apps/web/components/specific/auth/clerk/elements/already-have-acc.tsx
+++ apps/web/components/specific/auth/clerk/elements/already-have-acc.tsx
@@ -2,9 +2,9 @@ import Link from 'next/link';
 import React from 'react';
 
 const AlreadyHaveAcc = () => {
-	const url = process.env.NEXT_PUBLIC_CLERK_SIGN_IN_URL;
+	const url = process.env.NEXT_PUBLIC_CLERK_SIGN_IN_URL || '/auth/sign-in';
 
-	return <Link href={url!}>Already have an account?</Link>;
+	return <Link href={url}>Already have an account?</Link>;
 };
 
 export default AlreadyHaveAcc;
--- apps/web/components/specific/auth/clerk/elements/create-acc.tsx
+++ apps/web/components/specific/auth/clerk/elements/create-acc.tsx
@@ -2,9 +2,9 @@ import Link from 'next/link';
 import React from 'react';
 
 const CreateAcc = () => {
-	const url = process.env.NEXT_PUBLIC_CLERK_SIGN_UP_URL;
+	const url = process.env.NEXT_PUBLIC_CLERK_SIGN_UP_URL || '/auth/sign-up';
 
-	return <Link href={url!}>Create new account</Link>;
+	return <Link href={url}>Create new account</Link>;
 };
 
 export default CreateAcc;
