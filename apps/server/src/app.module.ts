409e901bab67611f0b85c71133d0dd02fd546e77
Akshay-Vs <me.akshay.vs@gmail.com>
2025-04-16 18:01:28 +0530

    feat(src/server): enhance NestJS server with security, documentation, and configuration

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          ttl: 60000,
+          limit: 10,
+        },
+      ],
+    }),
+    ConfigModule.forRoot()
+  ],
+  controllers: [
+    AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,8 +1,38 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger as NestLogger } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import helmet from 'helmet';
+
+const PORT = process.env.PORT ?? 4000;
+const ORIGIN = process.env.ORIGIN_URL?.trim()
+  ? process.env.ORIGIN_URL.trim().split(/\s+/)
+  : [];
+
+NestLogger.debug(`Using env: ${JSON.stringify({ port: PORT, origin: ORIGIN }, null, 2)}`, 'GLOBAL');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.use(helmet());
+  app.enableCors({
+    origin: ORIGIN,
+  });
+
+  const config = new DocumentBuilder().setTitle('Hwei Api Docs').setVersion('1.0').build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, documentFactory);
+
+  await app.listen(PORT);
+  NestLogger.log(`Application Port: ${PORT}`, '');
 }
-bootstrap();
+
+void (async (): Promise<void> => {
+  try {
+    await bootstrap();
+    NestLogger.log('Server Started', 'Bootstrap');
+  } catch (error) {
+    NestLogger.error(error, 'Bootstrap');
+  }
+})();
