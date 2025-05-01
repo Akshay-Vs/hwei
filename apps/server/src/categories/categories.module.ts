a1613c48ea439485266255fe1d103e7548ce3264
Akshay Vs <me.akshay.vs@gmail.com>
2025-05-01 23:44:54 +0530

    feat(apps/server): integrate PrismaService and RouterModule in CategoriesModule

--- apps/server/src/categories/categories.module.ts
+++ apps/server/src/categories/categories.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { CategoriesController } from './categories.controller';
 import { CategoriesService } from './categories.service';
+import { PrismaService } from 'src/common/database/prisma.service';
+import { RouterModule } from '@nestjs/core';
 
 @Module({
   controllers: [CategoriesController],
-  providers: [CategoriesService]
+  providers: [CategoriesService, PrismaService],
+  imports: [
+    RouterModule.register([
+      {
+        path: ':storeId',
+        module: CategoriesModule,
+      },
+    ]),
+  ],
 })
 export class CategoriesModule {}
