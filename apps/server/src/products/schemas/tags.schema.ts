f2f6966cc1e91dc181182df1d2bbf4213d159ba2
Akshay Vs <me.akshay.vs@gmail.com>
2025-05-07 23:56:21 +0530

    feat(apps/server): refine tags schema and DTOs for improved validation

--- apps/server/src/products/schemas/tags.schema.ts
+++ apps/server/src/products/schemas/tags.schema.ts
@@ -1,44 +1,35 @@
 import { createZodDto } from '@anatine/zod-nestjs';
 import { z } from 'zod';
 
-const tagsMetadataSchema = z.object({
+export const tagsMetadataSchema = z.object({
   id: z.string(),
 });
 
-const tagBaseSchema = z.object({
+export const tagBaseSchema = z.object({
   name: z.string().min(1),
 });
 
-const tagQuerySchema = z.object({
+export const tagInputSchema = z.object({
+  name: z.string().min(1).array(),
+});
+
+export const tagQuerySchema = z.object({
   search: z.string().optional(),
   skip: z.number().int().min(1).optional(),
   take: z.number().int().min(1).optional(),
 });
 
-const tagUpdateSchema = tagBaseSchema.partial();
+export const tagUpdateSchema = tagBaseSchema.partial();
 
-const tagSchema = tagsMetadataSchema.merge(tagBaseSchema);
+export const tagSchema = tagsMetadataSchema.merge(tagBaseSchema);
 
-type TagCreate = z.infer<typeof tagBaseSchema>;
-type TagUpdate = z.infer<typeof tagUpdateSchema>;
-type TagQuery = z.infer<typeof tagQuerySchema>;
-type TagMetadata = z.infer<typeof tagsMetadataSchema>;
-type Tag = z.infer<typeof tagSchema>;
+export type TagCreate = z.infer<typeof tagBaseSchema>;
+export type TagUpdate = z.infer<typeof tagUpdateSchema>;
+export type TagQuery = z.infer<typeof tagQuerySchema>;
+export type TagMetadata = z.infer<typeof tagsMetadataSchema>;
+export type Tag = z.infer<typeof tagSchema>;
 
-class TagCreateDto extends createZodDto(tagBaseSchema) {}
-class TagUpdateDto extends createZodDto(tagUpdateSchema) {}
-class TagQueryDto extends createZodDto(tagQuerySchema) {}
-class TagMetadataDto extends createZodDto(tagsMetadataSchema) {}
-
-export {
-  TagCreate,
-  TagUpdate,
-  TagQuery,
-  TagMetadata,
-  tagSchema,
-  Tag,
-  TagCreateDto,
-  TagUpdateDto,
-  TagQueryDto,
-  TagMetadataDto,
-};
+export class TagInputDto extends createZodDto(tagInputSchema) {}
+export class TagUpdateDto extends createZodDto(tagUpdateSchema) {}
+export class TagQueryDto extends createZodDto(tagQuerySchema) {}
+export class TagMetadataDto extends createZodDto(tagsMetadataSchema) {}
