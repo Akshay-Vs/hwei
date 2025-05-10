5206a1ac1eff6581ee8bb717e406d02c1afb5c97
Akshay Vs <me.akshay.vs@gmail.com>
2025-05-11 00:43:34 +0530

    feat(apps/server): integrate ProductsController and ProductsService into ProductsModule

--- apps/server/src/products/products.module.ts
+++ apps/server/src/products/products.module.ts
@@ -6,10 +6,32 @@ import { VariantsController } from './variants/variants.controller';
 import { VariantsService } from './variants/variants.service';
 import { PricesService } from './prices/prices.service';
 import { PricesController } from './prices/prices.controller';
+import { RouterModule } from '@nestjs/core';
+import { ProductsService } from './products.service';
+import { ProductsController } from './products.controller';
 
 @Module({
-  controllers: [TagsController, VariantsController, PricesController],
-  providers: [TagsService, PrismaService, VariantsService, PricesService],
+  controllers: [
+    TagsController,
+    VariantsController,
+    PricesController,
+    ProductsController,
+  ],
+  providers: [
+    TagsService,
+    PrismaService,
+    VariantsService,
+    PricesService,
+    ProductsService,
+  ],
   exports: [TagsService],
+  imports: [
+    RouterModule.register([
+      {
+        path: ':storeId',
+        module: ProductsModule,
+      },
+    ]),
+  ],
 })
 export class ProductsModule {}
