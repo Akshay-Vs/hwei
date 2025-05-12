54e9837a27e2ac32f24237a92ae1f88b39517c5e
Akshay Vs <me.akshay.vs@gmail.com>
2025-05-12 12:02:52 +0530

    feat(apps/server): add documentation decorators in CombinationController

--- apps/server/src/products/variants/combination/combination.controller.ts
+++ apps/server/src/products/variants/combination/combination.controller.ts
@@ -2,21 +2,24 @@ import { Controller, Get, Param } from '@nestjs/common';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { CombinationService } from './combination.service';
 import { PublicRoute } from 'src/common/decorators/public-route.decorator';
+import { FindAllDocs, FindOneDocs } from './combination.docs';
 
 @Controller('combination')
-@ApiTags('combination')
+@ApiTags('varient-combination')
 @ApiBearerAuth('swagger-access-token')
 export class CombinationController {
   constructor(private readonly combinationService: CombinationService) {}
 
   @Get()
   @PublicRoute()
+  @FindAllDocs()
   async findAll(@Param('productId') productId: string) {
     return await this.combinationService.findAll(productId);
   }
 
   @Get(':id')
   @PublicRoute()
+  @FindOneDocs()
   async findOne(
     @Param('productId') productId: string,
     @Param('id') id: string,
