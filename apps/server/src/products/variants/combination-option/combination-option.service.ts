5c107b2034faa891a4b0336f2b0ba776efb4b05c
Akshay Vs <me.akshay.vs@gmail.com>
2025-05-12 12:28:50 +0530

    feat(apps/server): implement CombinationOptionService with attach and detach methods

--- apps/server/src/products/variants/combination-option/combination-option.service.ts
+++ apps/server/src/products/variants/combination-option/combination-option.service.ts
@@ -1,4 +1,54 @@
-import { Injectable } from '@nestjs/common';
+import { Prisma } from '@/generated';
+import { Injectable, Logger } from '@nestjs/common';
+import { handleInternalError } from 'src/errors/handlers/internal.error.handler';
+import { CombinationOptionController } from './combination-option.controller';
 
 @Injectable()
-export class CombinationOptionService {}
+export class CombinationOptionService {
+  private readonly entity = 'CombinationOption';
+  private readonly logger = new Logger(CombinationOptionController.name);
+
+  async attatchCombinationOption(
+    tx: Prisma.TransactionClient,
+    combinationId: string,
+    optionId: string,
+  ): Promise<void> {
+    try {
+      await tx.variantCombinationOption.create({
+        data: {
+          combinationId,
+          optionId,
+        },
+      });
+    } catch (error) {
+      return handleInternalError({
+        error,
+        logger: this.logger,
+        entity: this.entity,
+      });
+    }
+  }
+
+  async detachCombinationOption(
+    tx: Prisma.TransactionClient,
+    combinationId: string,
+    optionId: string,
+  ): Promise<void> {
+    try {
+      await tx.variantCombinationOption.delete({
+        where: {
+          combinationId_optionId: {
+            combinationId,
+            optionId,
+          },
+        },
+      });
+    } catch (error) {
+      return handleInternalError({
+        error,
+        logger: this.logger,
+        entity: this.entity,
+      });
+    }
+  }
+}
