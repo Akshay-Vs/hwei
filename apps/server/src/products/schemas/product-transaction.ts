f879c682f76a531862549edb5e6f6435b5adf899
Akshay Vs <me.akshay.vs@gmail.com>
2025-06-11 16:17:08 +0530

    refactor(variants): move transaction variant to variant schema

--- apps/server/src/products/schemas/product-transaction.ts
+++ apps/server/src/products/schemas/product-transaction.ts
@@ -1,30 +1,13 @@
 import { z } from 'zod';
 import { productInputSchema } from './products.schema';
-import {
-  combinationInputSchema,
-  labelInputSchema,
-  optionInputSchema,
-} from './variants.schema';
+import { variantTransactionInput } from './variants.schema';
 import { imageInputSchema } from './images.schema';
-import { InventoryInputSchema } from './inventory.schema';
-import { priceInputSchema } from './price.schema';
 import { createZodDto } from '@anatine/zod-nestjs';
 
 export const productTransactionInput = z.object({
   metadata: productInputSchema,
   images: z.array(imageInputSchema.omit({ productId: true })),
-  variants: z.array(
-    z.object({
-      label: labelInputSchema.omit({ productId: true }),
-      items: z.array(
-        priceInputSchema
-          .omit({ combinationId: true })
-          .extend(InventoryInputSchema.omit({ combinationId: true }).shape)
-          .extend(optionInputSchema.omit({ variantLabelId: true }).shape)
-          .extend(combinationInputSchema.omit({ productId: true }).shape),
-      ),
-    }),
-  ),
+  variants: z.array(variantTransactionInput),
 });
 
 export type ProductTransactionInput = z.infer<typeof productTransactionInput>;
