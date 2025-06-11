2680d030c3797f324aff958b30ef912ebf41fabe
Akshay Vs <me.akshay.vs@gmail.com>
2025-06-11 16:19:27 +0530

    feat(products): add ProductTransactionsService for handling product transactions

---
+++ apps/server/src/products/product.transactions.service.ts
@@ -0,0 +1,45 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { PrismaService } from 'src/common/database/prisma.service';
+import { BaseService } from 'src/common/services/base.service';
+import { ProductTransactionInput } from './schemas/product-transaction';
+import { ImageService } from './image/image.service';
+import { VariantsService } from './variants/variants.service';
+import { ProductsService } from './products.service';
+
+@Injectable()
+export class ProductTransactionsService extends BaseService {
+  protected readonly logger = new Logger(ProductTransactionsService.name);
+  protected readonly entity = 'Product';
+
+  constructor(
+    protected readonly prisma: PrismaService,
+    protected readonly product: ProductsService,
+    protected readonly productImage: ImageService,
+    protected readonly variant: VariantsService,
+  ) {
+    super(prisma);
+  }
+
+  async createOne(storeId: string, input: ProductTransactionInput) {
+    await this.prisma.$transaction(async (tx) => {
+      // create product
+      const { id: productId } = await this.product.createOne(
+        tx,
+        storeId,
+        input.metadata,
+      );
+
+      // populate images
+      await this.productImage.createTransactionalImages(
+        tx,
+        productId,
+        input.images,
+      );
+
+      // populate variants
+      for (const variant of input.variants) {
+        await this.variant.handleVariant(tx, productId, variant);
+      }
+    });
+  }
+}
