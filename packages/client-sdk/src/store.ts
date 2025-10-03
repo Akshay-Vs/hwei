f711f7fec122e83cf316cf7fe30c0ce963caaad5
Akshay-Vs <me.akshay.vs@gmail.com>
2025-10-03 13:36:59 +0530

    feat: migrate schemas to shared library

---
+++ packages/client-sdk/src/store.ts
@@ -0,0 +1,10 @@
+
+import { Store } from "@hwei/schema/dto/store.schema"
+
+export const useStore = () => {
+  const createStore = () => { };
+  const getStore = (id: string) => { };
+  const getAllStores = () => { };
+  const updateStore = (id: string, data: any) => { };
+  const deleteStore = (id: string) => { };
+};
