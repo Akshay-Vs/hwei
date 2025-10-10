fb9ab01eaa11b24100a28f7d26a3393927f49eba
Akshay-Vs <me.akshay.vs@gmail.com>
2025-10-10 17:17:43 +0530

    feat: improve CORS handling & logging

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { Logger as NestLogger } from '@nestjs/common';
 import { AppModule } from './app.module';
-import helmet from 'helmet';
 import { setupSwagger } from './swagger';
+import { NextFunction } from 'express';
+import helmet from 'helmet';
 
 const PORT = process.env.PORT ?? 4000;
 const VERSION = process.env.VERSION ?? 'v1';
@@ -15,25 +16,32 @@ NestLogger.debug(
   'GLOBAL',
 );
 
+const logger = new NestLogger('Bootstrap');
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use(helmet());
   app.enableCors({
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     origin: ORIGIN,
+    credentials: true,
+  });
+
+  app.setGlobalPrefix(VERSION);
+
+  app.use((req: Request, _res: Response, next: NextFunction) => {
+    logger.debug(`Request: ${req.method} ${req.url} received`);
+    next();
   });
 
   setupSwagger(app);
-
-  app.setGlobalPrefix(VERSION);
 
   await app.listen(PORT);
   NestLogger.log(`Application Port: ${PORT}`, '');
 }
 
 void (async (): Promise<void> => {
-  const logger = new NestLogger('Bootstrap');
-
   try {
     await bootstrap();
     logger.log(
