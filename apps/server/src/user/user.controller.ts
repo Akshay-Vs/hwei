2476f37b997acb5bf34419a45026d648810de8a7
Akshay-Vs <me.akshay.vs@gmail.com>
2025-11-16 15:48:43 +0530

    refactor: streamline UserController by removing unused routes and simplifying methods

--- apps/server/src/user/user.controller.ts
+++ apps/server/src/user/user.controller.ts
@@ -4,7 +4,6 @@ import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { CacheInterceptor } from '@nestjs/cache-manager';
 import { User } from 'src/common/decorators/user.decorator';
 import { User as ClerkUser } from '@clerk/backend';
-import { StoreOwnershipGuard } from 'src/common/guards/store-ownership.guard';
 
 import {
   Body,
@@ -14,36 +13,17 @@ import {
   Param,
   Patch,
   Post,
-  Query,
-  UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
 
 import {
-  FindAllDocs,
-  FindOneDocs,
-  FindByClerkIdDocs,
-  FindByEmailDocs,
+  FindMeDocs,
   CreateOneDocs,
   UpdateOneDocs,
   DeleteOneDocs,
-  DeleteManyDocs,
-  UpdateRoleDocs,
 } from './user.docs';
 
-import {
-  PaginationQuery,
-  paginationQuerySchema,
-} from '@hwei/schema/dto/query-schema';
-
-import {
-  createUserSchema,
-  CreateUserDto,
-  updateUserSchema,
-  UpdateUserDto,
-  UpdateRoleDto,
-  updateRoleSchema,
-} from '@hwei/schema/dto/user.schema';
+import { updateUserSchema, UpdateUserDto } from '@hwei/schema/dto/user.schema';
 
 @ApiTags('users')
 @ApiBearerAuth('swagger-access-token')
@@ -52,83 +32,31 @@ import {
 export class UserController {
   constructor(private readonly userService: UserService) { }
 
-  @Get('me')
-  @FindOneDocs()
+  @Get('/me')
+  @FindMeDocs()
   async getMe(@User() user: ClerkUser) {
     return this.userService.findByClerkId(user.id);
   }
 
-  @Get('clerk/:clerkId')
-  @FindByClerkIdDocs()
-  async findByClerkId(@Param('clerkId') clerkId: string) {
-    return this.userService.findByClerkId(clerkId);
-  }
-
-  @Get('email/:email')
-  @FindByEmailDocs()
-  async findByEmail(@Param('email') email: string) {
-    return this.userService.findByEmail(email);
-  }
-
-  @Get(':id')
-  @FindOneDocs()
-  async findOne(@Param('id') id: string) {
-    return this.userService.findOne(id);
-  }
-
   @Post()
   @CreateOneDocs()
-  async create(
-    @Body(new ZodValidationPipe(createUserSchema))
-    input: CreateUserDto,
-  ) {
-    return this.userService.create(input);
+  async create(@User() user: ClerkUser) {
+    return this.userService.create(user);
   }
 
-  @Patch(':id')
+  @Patch()
   @UpdateOneDocs()
   async update(
-    @Param('id') id: string,
+    @User() user: ClerkUser,
     @Body(new ZodValidationPipe(updateUserSchema))
     input: UpdateUserDto,
   ) {
-    return this.userService.update(id, input);
+    return this.userService.update(user.id, input);
   }
 
-  @Delete(':id')
+  @Delete()
   @DeleteOneDocs()
-  async delete(@Param('id') id: string) {
-    return this.userService.delete(id);
+  async delete(@User() user: ClerkUser) {
+    return this.userService.delete(user.id);
   }
-
-  @Delete('/bulk')
-  @DeleteManyDocs()
-  async deleteBulk(@Body('ids') ids: string[]) {
-    return this.userService.deleteMany(ids);
-  }
-
-  //#endregion Admin Routes
-
-  @Get('')
-  @UseGuards(StoreOwnershipGuard)
-  @FindAllDocs()
-  async findAll(
-    @Query(new ZodValidationPipe(paginationQuerySchema))
-    pagination: PaginationQuery,
-  ) {
-    return this.userService.findAll(pagination);
-  }
-
-  @Patch('role/:id')
-  @UseGuards(StoreOwnershipGuard)
-  @UpdateRoleDocs()
-  async updateRole(
-    @Param('id') id: string,
-    @Body(new ZodValidationPipe(updateRoleSchema))
-    input: UpdateRoleDto,
-  ) {
-    return this.userService.updateRole(id, input.role);
-  }
-
-  ////#endregion
 }
